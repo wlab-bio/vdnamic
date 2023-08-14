c66c97974e11b4d5c001bd8f26fabc0f7827539a
Joshua Weinstein <jaweinst@gmail.com>
2023-08-14 18:01:39 -0500

    Add files via upload

---
+++ SAMPLE_DIRECTORY/numbers.sh
@@ -0,0 +1,1 @@
+python3 CODE_PATH/main.py gse -path NUMBER_SIMULATION// -max_eig_cuts 5 -inference_dim 2 -inference_eignum 50 -final_eignum 100 -iterations 1 -ncpus 5
---
+++ SAMPLE_DIRECTORY/seq-UEI.sh
@@ -0,0 +1,2 @@
+gunzip RAWDATA_PATH/*.fastq.gz
+python3 CODE_PATH/main.py lib UEI-directory/
