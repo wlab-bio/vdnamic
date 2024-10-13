600c9295b5448c5ed51d7a9edd54aebd4f645180
Joshua Weinstein <jaweinst@gmail.com>
2024-10-13 18:36:15 -0500

    Add files via upload

---
+++ SAMPLE_DIRECTORY/statue/statue_gse.sh
@@ -0,0 +1,7 @@
+gunzip link_assoc.txt.gz
+# non-hierarchical GSE
+python ../../main.py GSE -path ./ -inference_dim 3 -inference_eignum 25 -final_eignum 250
+# hierarchical GSE
+# python ../../main.py GSE -path ./ -inference_dim 3 -inference_eignum 25 -final_eignum 250 -sub_num 25 -sub_size 5000 -filter_criterion 0.0
+# hierarchical GSE with 0.2% filter
+# python ../../main.py GSE -path ./ -inference_dim 3 -inference_eignum 25 -final_eignum 250 -sub_num 25 -sub_size 5000 -filter_criterion 0.1,0.1
